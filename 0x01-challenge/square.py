bd5bbef858415ffea64bbe1212fbddcf62b776e0
ofhatwa <tofhatwa@gmail.com>
2024-07-15 20:20:16 +0200

    square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 
-class square():
-    
-    width = 0
-    height = 0
+class Square:
+    """Class representing a square"""
 
-    
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        """Initialize the square with given attributes"""
+        self.width = kwargs.get('width', 0)
+        self.height = kwargs.get('height', 0)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Calculate the area of the square"""
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+    def perimeter_of_my_square(self):
+        """Calculate the perimeter of the square"""
+        return self.width * 4
 
     def __str__(self):
+        """String representation of the square"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
